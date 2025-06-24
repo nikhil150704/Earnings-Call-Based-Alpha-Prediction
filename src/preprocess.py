b5d30fa4f2bc55938e7727dde49bdbfa8897647e
nikhil150704 <132289120+nikhil150704@users.noreply.github.com>
2025-06-25 01:52:34 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,5 @@
-
+if __name__ == "__main__":
+  prepare_text_for_nlp("data/raw/INFY_Q1_July_22.pdf", "current")
+  prepare_text_for_nlp("data/raw/INFY_Q2_October_21.pdf", "prev1")
+  prepare_text_for_nlp("data/raw/INFY_Q3_January_22.pdf", "prev2")
+  prepare_text_for_nlp("data/raw/INFY_Q4_April_22.pdf", "prev3")
